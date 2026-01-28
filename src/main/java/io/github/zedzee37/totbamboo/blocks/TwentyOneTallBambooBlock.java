bc4e05b8250717cf5f51c151d1a25a885ea57505
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2026-01-28 12:49:10 -0500

    mostly done

--- src/main/java/io/github/zedzee37/totbamboo/blocks/TwentyOneTallBambooBlock.java
+++ src/main/java/io/github/zedzee37/totbamboo/blocks/TwentyOneTallBambooBlock.java
@@ -1,20 +1,33 @@
 package io.github.zedzee37.totbamboo.blocks;
 
+import io.github.zedzee37.totbamboo.TTB;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class TwentyOneTallBambooBlock extends Block {
     private static final BooleanProperty IS_TOP_BAMBOO = BooleanProperty.create("is_top");
     private static final BlockState[] BLOCK_STATES = new BlockState[21];
+
+    // maybe a little stolen from BambooStalkBlock
+    private static final VoxelShape LARGE_SHAPE = Block.box((double)3.0F,
+            0.0F,
+            3.0F,
+            13.0F,
+            16.0F,
+            13.0F);
 
     // when the leaves start rendering ig
     private static final int LEAVES_START_POS = 18;
@@ -60,6 +73,38 @@ public class TwentyOneTallBambooBlock extends Block {
             BlockState blockState = BLOCK_STATES[i];
             level.setBlock(curPos, blockState, Block.UPDATE_ALL);
         }
+    }
+
+    @Override
+    protected @NotNull VoxelShape getShape(@NotNull BlockState state,
+                                           @NotNull BlockGetter level,
+                                           @NotNull BlockPos pos,
+                                           @NotNull CollisionContext context) {
+        return LARGE_SHAPE;
+    }
+
+    @Override
+    public @NotNull BlockState playerWillDestroy(@NotNull Level level,
+                                                 @NotNull BlockPos pos,
+                                                 @NotNull BlockState state,
+                                                 @NotNull Player player) {
+        if (!level.isClientSide()) {
+            // all connected bamboo needs to be destroyed
+            BlockPos abovePos = pos.above();
+            BlockPos belowPos = pos.below();
+            for (int i = 0; i < BLOCK_STATES.length; i++) {
+                if (level.getBlockState(abovePos).is(TTB.TWENTY_ONE_TALL_BAMBOO_BLOCK)) {
+                    level.removeBlock(abovePos, false);
+                    abovePos = abovePos.above();
+                }
+                if (level.getBlockState(belowPos).is(TTB.TWENTY_ONE_TALL_BAMBOO_BLOCK)) {
+                    level.removeBlock(belowPos, false);
+                    belowPos = belowPos.below();
+                }
+            }
+        }
+
+        return super.playerWillDestroy(level, pos, state, player);
     }
 
     @Override
---
+++ src/main/java/io/github/zedzee37/totbamboo/datagen/TTBBlockStateProvider.java
@@ -0,0 +1,20 @@
+package io.github.zedzee37.totbamboo.datagen;
+
+import io.github.zedzee37.totbamboo.TTB;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class TTBBlockStateProvider extends BlockStateProvider {
+    public TTBBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, TTB.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        final ModelFile twentyOneTallBambooModel = models().withExistingParent("twenty_one_tall_bamboo",
+                this.mcLoc("block/bamboo"));
+        simpleBlock(TTB.TWENTY_ONE_TALL_BAMBOO_BLOCK.get(), twentyOneTallBambooModel);
+    }
+}
